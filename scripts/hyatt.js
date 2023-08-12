7421e382421d96969d5dff15c631cc99d8f4b97b
mitchell3301 <mitchell.macijauskas1@marist.edu>
2023-08-12 18:51:18 -0400

    adding update button

--- scripts/hyatt.js
+++ scripts/hyatt.js
@@ -1,33 +1,28 @@
-// Hyatt makes some sort of external request that makes it so:
-// Even though page is 'loaded,' list of hotels isn't there
-// Workaround: Rerunning every .6 seconds
-
-// Need script to rerun when user drags across map
-// console.log("running");
-// let loader = document.querySelector(
-//   "body > div.m-content-loader.b-layer-interstitial"
-// );
-
-// console.log(loader.innerHTML);
-// loader.onchange = addCpp();
-
 // UPDATE BUTTON -------------------------------------------------------------
 const updateButton = document.createElement("button");
 updateButton.innerText = "Update CPP";
 updateButton.id = "clickToUpdate";
-
-// btn btn-primary-outline flex content-center px-4 py-2
 updateButton.setAttribute("class", "b-button b-button_height-mini b-mb0 b-ph5");
-
 document.querySelector(".hbe-header_woh").appendChild(updateButton);
 
 updateButton.addEventListener("click", () => {
-  document.onload = updateCpp();
+  updateCpp();
 });
 
-document
-  .querySelector(".search-this-area")
-  .addEventListener("click", updateCpp);
+document.querySelector(".search-this-area").addEventListener("click", () => {
+  const intervalSTA = setInterval(() => {
+    // Remove any overlapping cpp from radius
+    if (document.querySelectorAll(".cpp-rate").length > 0) {
+      console.log("Removing");
+      document.querySelectorAll(".cpp-rate").forEach((e) => e.remove());
+    }
+
+    if (document.querySelectorAll(".rate-currency").length > 0) {
+      clearInterval(intervalSTA);
+      addCpp();
+    }
+  }, 200);
+});
 
 // FUNCTION ----------------------------------------------------------------------
 const interval = setInterval(addCpp, 100);
@@ -76,33 +71,16 @@ function addCpp() {
       );
       span.innerText = "  Cents/Point";
 
-      // mainDiv.appendChild(div);
-      // mainDiv.appendChild(span);
-
-      // rates[i].appendChild(div);
-      // rates[i].appendChild(span);
       rates[i].appendChild(mainDiv);
       mainDiv.appendChild(div);
       mainDiv.appendChild(span);
-    }
-  }
-}
+    } // for
+  } // if
+} // addCpp()
 
 function updateCpp() {
-  //let onPage = document.querySelector(".tabular-nums").innerText.charAt(0);
-  //let nextPage = document.querySelector('#__next > main > div.mb-4.container-fluid > div.relative.flex > div.z-1.bg-bg.xl\:w-3\/7.relative.h-auto.w-full.rtl\:pr-0.md\:static.md\:h-auto.md\:w-1\/2.md\:pr-4.md\:rtl\:pl-4.lg\:w-2\/5 > div:nth-child(5) > div > div > button:nth-child(4)')
-  //console.log(onPage);
   if (document.querySelectorAll(".cpp-rate")) {
     document.querySelectorAll(".cpp-rate").forEach((e) => e.remove());
-    document.onload = addCpp();
-  }
-  // document
-  //   .querySelector(
-  //     "#__next > main > div.mb-4.container-fluid > div.relative.flex > div.z-1.bg-bg.xl:w-3/7.relative.h-auto.w-full.rtl:pr-0.md:static.md:h-auto.md:w-1/2.md:pr-4.md:rtl:pl-4.lg:w-2/5 > div:nth-child(5) > div > div > button:nth-child(4)"
-  //   )
-  //   .click();
-}
-
-// How to add class and then div under it
-
-// Polling
+  } // if
+  addCpp();
+} // updateCpp()
