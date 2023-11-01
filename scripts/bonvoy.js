ac11842e134f02de04b05d33b747eb1e9dc60418
mitchell3301 <mitchell.macijauskas1@marist.edu>
2023-11-01 19:04:57 -0400

    edit bonvoy to accomodate website remodel

--- scripts/bonvoy.js
+++ scripts/bonvoy.js
@@ -1,58 +1,74 @@
 // BONVOY HOTELS
+const interval = setInterval(addCpp, 600);
 
-function addCpp() {
-  let containers = document.querySelectorAll(".eeo-rate-not-enabled-class");
+addCpp(interval);
 
-  for (let i = 0; i < containers.length; i++) {
-    if (
-      containers[i].querySelector("span.t-points.t-point-saver-point") &&
-      containers[i].querySelector(
-        "span.price-night.t-font-xs.t-font-weight-semibold.t-price-night.with-points > span > span > span.t-price"
-      )
-    ) {
-      // make sure both cash and points options exist
-      let cpp = (
-        (parseInt(
-          containers[i]
-            .querySelector(
-              "span.price-night.t-font-xs.t-font-weight-semibold.t-price-night.with-points > span > span > span.t-price"
-            )
-            .innerText.replace(",", "")
-        ) /
-          parseInt(
+function addCpp(interval) {
+  console.log("GOIN");
+
+  // remove any cpp previously added
+  if (document.querySelectorAll(".with-points")) {
+    document.querySelectorAll(".with-points").forEach((e) => e.remove());
+  } // if
+
+  let containers = document.querySelectorAll(".non-saver");
+  console.log(containers);
+
+  if (containers.length > 0) {
+    if (interval) {
+      clearInterval(interval);
+    }
+
+    for (let i = 0; i < containers.length; i++) {
+      console.log(containers[i]);
+      if (
+        containers[i].querySelector(".price-section") &&
+        containers[i].querySelector(".m-price-currency-s")
+      ) {
+        console.log("ZZZ");
+        // make sure both cash and points options exist
+        let cpp = (
+          (parseInt(
             containers[i]
-              .querySelector("span.t-points.t-point-saver-point")
+              .querySelector(".price-value")
               .innerText.replace(",", "")
-          )) *
-        100
-      ).toFixed(2);
+          ) /
+            parseInt(
+              containers[i]
+                .querySelector(".points-value")
+                .innerText.replace(",", "")
+            )) *
+          100
+        ).toFixed(2);
 
-      const outerSpan = document.createElement("span");
-      const middleSpan = document.createElement("span");
-      const innerSpan = document.createElement("span");
-      const span = document.createElement("span");
+        const outerDiv = document.createElement("div");
+        const firstChild = document.createElement("div");
+        const secondChild = document.createElement("div");
 
-      outerSpan.setAttribute(
-        "class",
-        "price-night t-font-xs t-font-weight-semibold t-price-night with-points"
-      );
+        outerSpan.setAttribute(
+          "class",
+          "currency-section m-price-currency-s with-points"
+        );
 
-      innerSpan.setAttribute("class", "non-eeo-price-container");
+        firstChild.setAttribute(
+          "class",
+          "t-font-alt-xs price-value m-price-currency-s"
+        );
 
-      span.setAttribute(
-        "class",
-        "t-font-xs t-font-weight-semibold t-price-night with-points"
-      );
+        secondChild.setAttribute(
+          "class",
+          "t-font-alt-xs currency-label m-price-currency-s"
+        );
 
-      span.innerHTML = cpp + " Cents/Point&nbsp;";
+        span.innerHTML = cpp + " Cents/Point&nbsp;";
 
-      outerSpan.appendChild(middleSpan);
-      middleSpan.appendChild(innerSpan);
-      innerSpan.appendChild(span);
+        outerDiv.appendChild(firstChild)
+        outerDiv.appendChild(secondChild)
 
-      containers[i].appendChild(outerSpan);
-    } // If
-  } // For
+        containers[i].appendChild(outerDiv;
+      } // If
+    } // For
+  } // If
 }
 
 document.onload = addCpp();
